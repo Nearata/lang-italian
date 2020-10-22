0264b4aa6cd9d13df72cb88173d0c9b3a2278904
William Di Cicco <nearata@protonmail.com>
2020-10-22 15:27:44 +0200

    fix: Switch to dayjs locale

--- locale/config.js
+++ locale/config.js
@@ -1,44 +1,28 @@
-// https://github.com/moment/moment/blob/develop/locale/it.js
+// https://github.com/iamkun/dayjs/blob/v1.9.3/src/locale/it.js
 
-moment.locale('it', {
+dayjs.locale({
+	name: 'it',
+	weekdays: 'domenica_lunedì_martedì_mercoledì_giovedì_venerdì_sabato'.split('_'),
+	weekdaysShort: 'dom_lun_mar_mer_gio_ven_sab'.split('_'),
+	weekdaysMin: 'do_lu_ma_me_gi_ve_sa'.split('_'),
 	months: 'gennaio_febbraio_marzo_aprile_maggio_giugno_luglio_agosto_settembre_ottobre_novembre_dicembre'.split('_'),
+	weekStart: 1,
 	monthsShort: 'gen_feb_mar_apr_mag_giu_lug_ago_set_ott_nov_dic'.split('_'),
-	weekdays: 'Domenica_Lunedì_Martedì_Mercoledì_Giovedì_Venerdì_Sabato'.split('_'),
-	weekdaysShort: 'Dom_Lun_Mar_Mer_Gio_Ven_Sab'.split('_'),
-	weekdaysMin: 'Do_Lu_Ma_Me_Gi_Ve_Sa'.split('_'),
-	longDateFormat: {
+	formats: {
 		LT: 'HH:mm',
 		LTS: 'HH:mm:ss',
 		L: 'DD/MM/YYYY',
 		LL: 'D MMMM YYYY',
 		LLL: 'D MMMM YYYY HH:mm',
-		LLLL: 'dddd, D MMMM YYYY HH:mm'
-	},
-	calendar: {
-		sameDay: '[Oggi alle] LT',
-		nextDay: '[Domani alle] LT',
-		nextWeek: 'dddd [alle] LT',
-		lastDay: '[Ieri alle] LT',
-		lastWeek: function () {
-			switch (this.day()) {
-				case 0:
-					return '[la scorsa] dddd [alle] LT';
-				default:
-					return '[lo scorso] dddd [alle] LT';
-			}
-		},
-		sameElse: 'L'
+		LLLL: 'dddd D MMMM YYYY HH:mm'
 	},
 	relativeTime: {
-		future: function (s) {
-			return ((/^[0-9].+$/).test(s) ? 'tra' : 'in') + ' ' + s;
-		},
+		future: 'tra %s',
 		past: '%s fa',
-		s: 'alcuni secondi',
-		ss: '%d secondi',
+		s: 'qualche secondo',
 		m: 'un minuto',
 		mm: '%d minuti',
-		h: 'un\'ora',
+		h: 'un\' ora',
 		hh: '%d ore',
 		d: 'un giorno',
 		dd: '%d giorni',
@@ -47,10 +31,5 @@ moment.locale('it', {
 		y: 'un anno',
 		yy: '%d anni'
 	},
-	dayOfMonthOrdinalParse: /\d{1,2}º/,
-	ordinal: '%dº',
-	week: {
-		dow: 1, // Monday is the first day of the week.
-		doy: 4  // The week that contains Jan 4th is the first week of the year.
-	}
-});
+	ordinal: '%dº'
+}, null, false);
